eb1185b954ef94ab4ee845e579fa36a0d6db1e09
Shawn Rice <iamshawnrice@gmail.com>
2024-12-01 21:17:05 -0500

    Install and configure chakra-ui

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "app/components/ui/button";
+import { HStack } from "@chakra-ui/react";
+
 export default function Admin() {
   return (
     <section>
       <h1>Hello from the Admin Page</h1>
+
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
     </section>
   );
 }
